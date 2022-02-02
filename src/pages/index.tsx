8f94b67e981c591d7e2ee0510e7aa434b0ee4b52
Jordan Sleeper <jordanpsleeper@gmail.com>
2022-02-02 18:59:09 -0500

    BG color change for fast rebuild

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const Hello = styled.div`background: blue;`;
+const Hello = styled.div`
+  background: purple;
+  color: #000;
+`;
 
 const Homepage: React.FC = () => {
   return <Hello>Hello, World!</Hello>
